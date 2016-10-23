a3eebbd86d8880410a22cc5bf4f5b926c43a617d
imccausl <imccausl@icloud.com>
2016-10-23 01:16:11 -0400

    moved the methods of the cat object responsible for rendering the view and incrementing the count into the MVC logic.

--- scalable-code-demo/js/app.js
+++ scalable-code-demo/js/app.js
@@ -1,22 +1,22 @@
 /* app.js */
 
 (function() { 	
-	var data = {
+	var model = {
 		cats: {},
+		currentCat: {},
 		defaultCats: []
 		
 	};
 
-	var view = {
+	var catDropdownView = {
 		init: function() {
 			/* select cat dropdown logic */
 			$("#ct-cat-selector a").on("click", function(e) {
 				e.preventDefault();
 				var selectedCat = $(this).text().toLowerCase();
 				
-				controller.selectCat(selectedCat);
-				
-				console.log(selectedCat);
+				controller.setCurrentCat(model.cats[selectedCat]);
+				view.render();
 			});
 		},
 		
@@ -25,8 +25,40 @@
 		}
 	};
 	
+	var view = {
+		init: function() {
+			// dom elements saved into the view object for easy reference.
+			this.catElem = document.getElementById("cat");
+			this.catNameElem = document.getElementById("cat-name");
+			this.catImgElem = document.getElementById("cat-img");
+			this.countElem = document.getElementById("cat-count");
+			
+			// listener for clicking on the cat to increase the click count.
+			this.catImgElem.addEventListener("click", function() {
+				controller.incrementCounter();
+			});
+			
+			// after the click count increases, render the view.
+			view.render();
+		},
+		
+		render: function() {
+			// update the DOM elements with the current cat information.
+			var currentCat = controller.getCurrentCat();
+			
+			console.log("New rendering the view with: " + currentCat.imgSrc);
+			this.countElem.textContent = currentCat.catName + " has been clicked "+ currentCat.clickCount + " times.";
+			this.catNameElem.textContent = currentCat.catName;
+			this.catImgElem.src = currentCat.imgSrc;
+		}
+	};
+	
 	var controller = {
 		createDefaultCats: function(names) {
+			// This function creates a number of new cats using my Cat constructor function and an array of names
+			// passed to it that were parsed from the HTML of the cat selector menu. Each list menu item
+			// becomes a cat.
+			
 			var imagePath = "";
 			var name = "";
 			
@@ -34,52 +66,51 @@
 				imagePath = names[i] + ".jpg";
 				name = names[i];
 				
-				data.cats[name] = new Cat(imagePath, name);
+				model.cats[name] = new Cat(imagePath, name);
 			}
 			
-			console.log(data.cats);
+			console.log(model.cats);
 		},
 		
 		parseCatNamesFromMenu: function() {
 			/* grab items from the cat selector menu */
 			var catNamesMenu = document.querySelectorAll("#ct-cat-selector a");
 			
+			// initialize the defaultCats list just in case.
+			model.defaultCats = [];
+			
 			/* put the text of these items into an array. This array will be
 			   create the names of the 5 cats that comprise cat clicker premium */
 			for (var i = 0; i<catNamesMenu.length; i++) {
-				data.defaultCats.push(catNamesMenu[i].textContent.toLowerCase());
+				model.defaultCats.push(catNamesMenu[i].textContent.toLowerCase());
 			}
-			
-			console.log(data.defaultCats);
 		},
 		
-		selectCat: function(cat) {
-			var numberOfCats = Object.keys(data.cats).length;
-			
-			for (var key in data.cats) {
-				data.cats[key].currentCat = false;
-			}
-			
-			controller.removeCats();
-			data.cats[cat].displayCat();
+		setCurrentCat: function(cat) {
+			model.currentCat = cat;
+			console.log("The variable passed into setCurrentCat function is: " + cat);
+			console.log("Current cat set to: " + model.currentCat);
 		},
 		
-		removeCats: function() {
-			var catDisplayArea = document.querySelector("[data-view='cat-detail']");
-			var countDisplayArea = document.querySelector("[data-view='click-count']");
-			
-			catDisplayArea.innerHTML = "";
-			countDisplayArea.innerHTML = "";
+		getCurrentCat: function() {
+			return model.currentCat;	
+		},
+				
+		incrementCounter: function() {
+			model.currentCat.clickCount++;
+			view.render();	
 		},
 		
 		init: function() {
 			controller.parseCatNamesFromMenu();
-			controller.createDefaultCats(data.defaultCats);
-			data.cats[data.defaultCats[0]].displayCat();
+			controller.createDefaultCats(model.defaultCats);
+			controller.setCurrentCat(model.cats[model.defaultCats[0]]);
+			
+			catDropdownView.init();
+			view.init();
 		}
 		
 	};
 	
 	controller.init();
-	view.init();
 })();
